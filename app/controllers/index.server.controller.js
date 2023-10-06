3079cf9c96487c2959d6a52eb69a14201f2fe969
Marcus Charles <Sucrammoses@gmail.com>
2023-10-05 20:07:32 -0400

    first commit

---
+++ app/controllers/index.server.controller.js
@@ -0,0 +1,6 @@
+exports.render = function (req,res){
+    res.render('index',{
+        /*controllers.js,Marcus Charles,301015342, October 5,2023*/
+        title:'New Express Application'
+    })
+};
